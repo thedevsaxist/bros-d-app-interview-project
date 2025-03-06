8c03a1f5437ec0a3b6fc32cd7a48525cc48fd145
Chidiebube Iroezindu <chidiebubeiroezindu@gmail.com>
2025-03-06 16:50:35 +0100

    Update border radius constant from 20 to 12 for improved UI consistency

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -4,4 +4,4 @@ const EdgeInsets kPadding20 = EdgeInsets.all(20);
 const EdgeInsets kPaddingRight20 = EdgeInsets.only(right: 20);
 const EdgeInsets kPaddingRight30 = EdgeInsets.only(right: 30);
 const EdgeInsets kPaddingRight80 = EdgeInsets.only(right: 80);
-final BorderRadius kBorderRadius = BorderRadius.circular(20);
+final BorderRadius kBorderRadius = BorderRadius.circular(12);
